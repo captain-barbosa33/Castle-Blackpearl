7966d301ba95d5cc07a6642f8b19004b10246b72
Barbosa <barbosahacks@gmail.com>
2024-12-01 12:35:20 -0500

    2024-12-01 12:35:20

--- test1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/barbosa/Castle-Blackpearl
-$SHELL
-git add .
-commit_message="${1:-'Automated commit'}"
-git commit -m "$commit_message"
-#git commit -m "$date"
-git push -u origin main 
